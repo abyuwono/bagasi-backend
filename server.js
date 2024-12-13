e5d4fa2601da441976376d35a56e6edc04697820
Budi Yuwono <budi.yuwono@mivo.com>
2024-12-13 20:21:13 +1100

    Update CSP and add explicit CORS headers for better cross-origin support

--- server.js
+++ server.js
@@ -28,8 +28,17 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; connect-src 'self' http://localhost:5001 https://api.stripe.com https://m.stripe.network; script-src 'self' https://js.stripe.com 'unsafe-inline' 'unsafe-eval' blob:; frame-src 'self' https://js.stripe.com https://hooks.stripe.com"
+    "default-src 'self'; " +
+    "connect-src 'self' http://localhost:5001 https://bagasi-backend-production.up.railway.app https://api.stripe.com https://m.stripe.network; " +
+    "script-src 'self' https://js.stripe.com 'unsafe-inline' 'unsafe-eval' blob:; " +
+    "frame-src 'self' https://js.stripe.com https://hooks.stripe.com; " +
+    "img-src 'self' data: https: http:; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+    "font-src 'self' https://fonts.gstatic.com"
   );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
